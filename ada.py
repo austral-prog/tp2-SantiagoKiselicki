44a4113b16645fcb3e88b12331d90f2ea397a788
Diego Baldassare <diegobaldassare@gmail.com>
2025-09-13 22:15:17 -0300

    Hand-in by email

--- ada.py
+++ ada.py
@@ -1,3 +1,14 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+
+    primero = first_name.lower() + " " + last_name.lower()
+    segundo = first_name.title() + " " + last_name.title()
+    ultimo = first_name.upper() + " " + last_name.upper()
+
+    print (primero)
+    print (segundo)
+    print (ultimo)
+    print ("\t" + primero)
+   
+ada()
