aee6d6aa58fac75c3dbfbffefc5891cd8bf06237
Diego Baldassare <diegobaldassare@gmail.com>
2025-09-13 22:15:46 -0300

    Hand-in by email

--- change.py
+++ change.py
@@ -1,3 +1,20 @@
-def change():
-    expense = 23.75
-    money = 100
+def change ():
+     expense = 23.75
+     money = 100
+     vuelto = money - expense
+     pesos = int(vuelto)
+     centavos = vuelto - pesos
+     resultado_centavo = int(centavos*100)
+     print (f"Ingresar gasto:")
+     print (expense)
+     print (f"Dinero recibido")
+     print (money)
+     print ("")
+     print ("Vuelto")
+     print ("")
+     print (f"Pesos:")
+     print (pesos)
+     print (f"Centavos:")
+     print (resultado_centavo)
+
+change()
