49cfdea39c88e17ab74857570a506844b33f94c7
Diego Baldassare <diegobaldassare@gmail.com>
2025-09-13 22:16:06 -0300

    Hand-in by email

--- earth.py
+++ earth.py
@@ -1,3 +1,9 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
+
+    if x < y:
+        print (f"The result of {x} comes first in the dictionary than {y} is {True}.")
+    if x < y:
+        print (f"The result of {y} comes    first in the dictionary than {x} is {False}.")
+earth()   
